5a2c5ad7af454cbeaa2be32a11beaea3778d2de1
Apple <appleeyespackage@gmail.com>
2023-09-29 12:40:48 +0100

    feat: api endpoints defined

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1/api')->group(function () {
+    Route::post('/upload', [VideoController::class, 'upload']);
+    Route::get('/play/{video}',
+        [VideoController::class, 'play']
+    );
 });
