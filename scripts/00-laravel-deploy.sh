d62bdcbe9b3ad1bafca0243bf7f0cbe167a78acc
Apple <appleeyespackage@gmail.com>
2023-10-18 20:30:55 +0100

    feat: create storage link

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,14 +3,14 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
-php artisan key:generate --show
-
 echo "Caching config..."
 php artisan config:cache
 
 echo "Caching routes..."
 php artisan route:cache
+
+echo "Create storage link..."
+php artisan storage:link
 
 echo "Running migrations..."
 php artisan migrate --force
